15ee58b7ec11130cac18a4a047c253c2390795a3
Boozmanras <paulvictoraa@gmail.com>
2023-08-03 05:04:43 -0700

    Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,16 +14,17 @@ if (*s1 == '\0' && *s2 == '\0')
 {
 return (1);
 }
-else if (*s1 == *s2 || *s2 == '*')
-{
-return (wildcmp(s1 + 1, s2 + 1));
-}
-else if (*s2 == '*')
-{
-return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
-}
-else
+if (*s2 == '\0')
 {
 return (0);
 }
+if (*s1 == *s2 || *s2 == '*')
+{
+return (wildcmp(s1 + 1, s2 + 1));
+}
+if (*s2 != '*' && *(s2 + 1) == '*')
+{
+return (wildcmp(s1, s2 + 1));
+}
+return (0);
 }
