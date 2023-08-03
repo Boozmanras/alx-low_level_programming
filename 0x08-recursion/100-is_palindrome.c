32a2dadbac9928e3075f856795cb74dfda5f1e11
Boozmanras <paulvictoraa@gmail.com>
2023-08-03 04:35:24 -0700

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * is_palindrome - Checks if a string is a palindrome using recursion.
+ * @s: The input string to check.
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+int len = strlen(s);
+if (len < 1)
+{
+return (1);
+}
+if (s[0] != s[len - 1])
+{
+return (0);
+}
+s[len - 1] = '\0';
+return (is_palindrome(s + 1));
+}
