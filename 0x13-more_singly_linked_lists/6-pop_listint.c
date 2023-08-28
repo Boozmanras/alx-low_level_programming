f53977ce3d108ddd39966d2dc0fa2322ac2abdeb
Boozmanras <paulvictoraa@gmail.com>
2023-08-28 10:20:25 -0700

    Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,15 +7,11 @@
 */
 int pop_listint(listint_t **head)
 {
-
-int data;
-listint_t *temp;
-
 if (*head == NULL)
 return (0);
 
-data = (*head)->n;
-*temp = *head;
+int data = (*head)->n;
+listint_t *temp = *head;
 *head = (*head)->next;
 free(temp);
 
