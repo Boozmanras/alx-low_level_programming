c1c4cf011161cdc513d8ab43e23db615ca6e6fa0
Boozmanras <paulvictoraa@gmail.com>
2024-01-10 11:25:14 -0800

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,70 +1,48 @@
 #include "main.h"
-#include <string.h>
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
 
 /**
-* is_valid_char - Checks if a character is a valid alphanumeric character
-* @c: Character to check
-*
-* Return: 1 if valid, 0 if not
-*/
-int is_valid_char(char c)
-{
-return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'));
-}
-
-/**
-* check_palindrome - Checks if a string is a palindrome recursively
-* @s: String to check
-* @start: Start index of the string
-* @end: End index of the string
-*
-* Return: 1 if palindrome, 0 if not
-*/
-int check_palindrome(char *s, int start, int end)
-{
-if (start >= end)
-{
-return (1);
-}
-
-if (s[start] != s[end])
-{
-return (0);
-}
-
-return (check_palindrome(s, start + 1, end - 1));
-}
-
-/**
-* is_palindrome - Checks if a string is a palindrome
-* @s: String to check
-*
-* Return: 1 if palindrome, 0 if not
-*/
+ * is_palindrome - Checks if a string is a palindrome
+ * @s: String to check
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
 int is_palindrome(char *s)
 {
-int length = strlen(s);
-int start = 0, end = length - 1;
-
-while (start < end)
-{
-while (!is_valid_char(s[start]) && start < end)
-{
-start++;
-}
-while (!is_valid_char(s[end]) && end > start)
-{
-end--;
+if (*s == 0)
+return (1);
+return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
-if (start < end && s[start] != s[end])
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: String to find the length of
+ *
+ * Return: Length of the string
+ */
+
+int _strlen_recursion(char *s)
 {
+if (*s == '\0')
 return (0);
+return (1 + _strlen_recursion(s + 1));
 }
 
-start++;
-end--;
-}
-
-return (check_palindrome((s, 0, length - 1)));
+/**
+ * check_pal - Recursively checks characters for palindrome
+ * @s: String to check
+ * @i: Iterator
+ * @len: Length of the string
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+int check_pal(char *s, int i, int len)
+{
+if (*(s + i) != *(s + len - 1))
+return (0);
+if (i >= len)
+return (1);
+return (check_pal(s, i + 1, len - 1));
 }
