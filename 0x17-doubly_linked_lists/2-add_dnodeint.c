db535e411b544e9e3e36bab30452a51d5e1265c5
Boozmanras <paulvictoraa@gmail.com>
2024-08-14 21:33:17 +0300

    changed made

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,26 +1,38 @@
 #include "lists.h"
 
 /**
-* add_dnodeint - adds a new node at the beginning of a dlistint_t list
-* @head: double pointer to the head of the list
-* @n: data to insert in the new node
-* Return: address of the new element, or NULL if it failed
+* add_dnodeint - adds a new node at start
+* of a dlistint_t list
+*
+* @head: start of the list
+* @n: value for element
+* Return: the address of new element
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *new_node;
+dlistint_t *new;
+dlistint_t *h;
 
-new_node = malloc(sizeof(dlistint_t));
-if (!new_node)
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
 return (NULL);
 
-new_node->n = n;
-new_node->prev = NULL;
-new_node->next = *head;
+new->n = n;
+new->prev = NULL;
+h = *head;
 
-if (*head)
-(*head)->prev = new_node;
+if (h != NULL)
+{
+while (h->prev != NULL)
+h = h->prev;
+}
 
-*head = new_node;
-return (new_node);
+new->next = h;
+
+if (h != NULL)
+h->prev = new;
+
+*head = new;
+
+return (new);
 }
--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,38 +1,54 @@
 #include "lists.h"
 
 /**
-* insert_dnodeint_at_index - inserts a new node at a given position
-* @h: double pointer to the head of the list
-* @idx: index where the new node should be added
-* @n: data to insert in the new node
-* Return: address of the new node, or NULL if it failed
+* insert_dnodeint_at_index - inserts a new node at
+* a given position
+*
+* @h: head of the list
+* @idx: index of the new node
+* @n: value of the new node
+* Return: the address of the new node, or NULL if it failed
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new_node, *current;
+dlistint_t *new;
+dlistint_t *head;
 unsigned int i;
 
+new = NULL;
 if (idx == 0)
-return (add_dnodeint(h, n));
+new = add_dnodeint(h, n);
+else
+{
+head = *h;
+i = 1;
+if (head != NULL)
+while (head->prev != NULL)
+head = head->prev;
+while (head != NULL)
+{
+if (i == idx)
+{
+if (head->next == NULL)
+new = add_dnodeint_end(h, n);
+else
+{
+new = malloc(sizeof(dlistint_t));
+if (new != NULL)
+{
+new->n = n;
+new->next = head->next;
+new->prev = head;
+head->next->prev = new;
+head->next = new;
+}
+}
+break;
+}
+head = head->next;
+i++;
+}
+}
 
-current = *h;
-for (i = 0; current && i < idx - 1; i++)
-current = current->next;
-
-if (!current)
-return (NULL);
-
-new_node = malloc(sizeof(dlistint_t));
-if (!new_node)
-return (NULL);
-
-new_node->n = n;
-new_node->next = current->next;
-new_node->prev = current;
-
-if (current->next)
-current->next->prev = new_node;
-
-current->next = new_node;
-return (new_node);
+return (new);
 }
